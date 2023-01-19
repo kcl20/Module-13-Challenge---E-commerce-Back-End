a2df359c3059b9c24437cf16ce2cf83f9bb76831
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-01-19 15:49:38 -0500

    Updated models.js files to successfully seed data into schema.

--- db/schema.sql
+++ db/schema.sql
@@ -7,42 +7,42 @@ CREATE DATABASE ecommerce_db;
 -- Use the ecommerce_db --
 USE ecommerce_db;
 
--- Creates the table "categories" within ecommerce_db --
-CREATE TABLE categories (
-    -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows, Doesn't allow null values --
-    id INT NOT NULL AUTO_INCREMENT,
-    -- Makes a string column called "name" which cannot contain null --
-    category_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-    );
+-- -- Creates the table "category" within ecommerce_db --
+-- CREATE TABLE category (
+--     -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows, Doesn't allow null values --
+--     id INT NOT NULL AUTO_INCREMENT,
+--     -- Makes a string column called "name" which cannot contain null --
+--     category_name VARCHAR(100) NOT NULL,
+--     PRIMARY KEY (id)
+--     );
 
 
--- one to many relationship between categories and products, categories can have many products but products can only have one category --
+-- -- one to many relationship between categories and products, categories can have many products but products can only have one category --
 
-CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock INT NOT NULL DEFAULT 10,
-    category_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-    );
-
-
-CREATE TABLE tags (
-    id INT NOT NULL AUTO_INCREMENT,
-    tag_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-    );
+-- CREATE TABLE product (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     product_name VARCHAR(100) NOT NULL,
+--     price DECIMAL(10,2) NOT NULL,
+--     stock INT NOT NULL DEFAULT 10,
+--     category_id INT NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (category_id) REFERENCES category(id)
+--     );
 
 
--- joining table linking products to potentially many tags, many to many --
-CREATE TABLE product_tags (
-    id INT NOT NULL AUTO_INCREMENT,
-    product_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (product_id) REFERENCES products(id),
-    FOREIGN KEY (tag_id) REFERENCES tags(id)
-    );
+-- CREATE TABLE tag (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     tag_name VARCHAR(100) NOT NULL,
+--     PRIMARY KEY (id)
+--     );
+
+
+-- -- joining table linking products to potentially many tags, many to many --
+-- CREATE TABLE productTag (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     product_id INT NOT NULL,
+--     tag_id INT NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (product_id) REFERENCES product(id),
+--     FOREIGN KEY (tag_id) REFERENCES tag(id)
+--     );
